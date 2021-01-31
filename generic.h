3f2cd01ed5a0bc48dccd0995fa65c40a0c583568
keddelzz <keddelzz@web.de>
2021-01-31 18:58:20 +0100

    Add generic representation for classes and class-members

--- generic.h
+++ generic.h
@@ -47,3 +47,27 @@ typename Generic<T>::Type to_value(
 {
     return to_value<T>(Generic<T>(), repr);
 }
+
+template<typename T>
+class Class {
+public:
+    constexpr explicit Class(const char *name, T value)
+        : name(name)
+        , value(value)
+    {}
+
+    const char *name;
+    T          value;
+};
+
+template<typename T>
+class Member {
+public:
+    constexpr Member(const char *name, T value)
+        : name(name)
+        , value(value)
+    {}
+
+    const char *name;
+    T           value;
+};
