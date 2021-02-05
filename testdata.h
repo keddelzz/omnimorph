0aba6ddc33c80a1387177970e3480fb48cf3b8cc
keddelzz <keddelzz@web.de>
2021-02-05 17:03:02 +0100

    Add two structurally equal vector implementations

--- testdata.h
+++ testdata.h
@@ -79,3 +79,81 @@ struct Generic<TestPerson>
         );
     };
 };
+
+struct Vec3
+{
+    Vec3(float x, float y, float z)
+        : x(x)
+        , y(y)
+        , z(z)
+    {}
+    float x, y, z;
+};
+
+using Vec3_Repr_3 = HNil;
+using Vec3_Repr_2 = HList<Member<float>, Vec3_Repr_3>;
+using Vec3_Repr_1 = HList<Member<float>, Vec3_Repr_2>;
+using Vec3_Repr_0 = HList<Member<float>, Vec3_Repr_1>;
+using Vec3_Repr   = Class<Vec3, Vec3_Repr_0>;
+template<>
+struct Generic<Vec3>
+{
+    using Type = Vec3;
+    using Repr = Vec3_Repr;
+    constexpr Generic() = default;
+    static Repr to(const Type &value) {
+        constexpr auto tl2 = Vec3_Repr_2(member_of(Vec3, x), {});
+        constexpr auto tl1 = Vec3_Repr_1(member_of(Vec3, y), tl2);
+        constexpr auto tl0 = Vec3_Repr_0(member_of(Vec3, z), tl1);
+        return Vec3_Repr("Vec3", FlatPointer(&value), tl0);
+    }
+    static Type from(const Repr &repr) {
+        const auto &tl0 = repr.members;
+        const auto &tl1 = tl0.tail;
+        const auto &tl2 = tl1.tail;
+        return Vec3(
+            *tl0.head.of(repr.base),
+            *tl1.head.of(repr.base),
+            *tl2.head.of(repr.base)
+        );
+    };
+};
+
+struct Vac3
+{
+    Vac3(float x, float y, float z)
+        : x(x)
+        , y(y)
+        , z(z)
+    {}
+    float x, y, z;
+};
+
+using Vac3_Repr_3 = HNil;
+using Vac3_Repr_2 = HList<Member<float>, Vac3_Repr_3>;
+using Vac3_Repr_1 = HList<Member<float>, Vac3_Repr_2>;
+using Vac3_Repr_0 = HList<Member<float>, Vac3_Repr_1>;
+using Vac3_Repr   = Class<Vac3, Vac3_Repr_0>;
+template<>
+struct Generic<Vac3>
+{
+    using Type = Vac3;
+    using Repr = Vac3_Repr;
+    constexpr Generic() = default;
+    static Repr to(const Type &value) {
+        constexpr auto tl2 = Vac3_Repr_2(member_of(Vac3, x), {});
+        constexpr auto tl1 = Vac3_Repr_1(member_of(Vac3, y), tl2);
+        constexpr auto tl0 = Vac3_Repr_0(member_of(Vac3, z), tl1);
+        return Vac3_Repr("Vac3", FlatPointer(&value), tl0);
+    }
+    static Type from(const Repr &repr) {
+        const auto &tl0 = repr.members;
+        const auto &tl1 = tl0.tail;
+        const auto &tl2 = tl1.tail;
+        return Vac3(
+                *tl0.head.of(repr.base),
+                *tl1.head.of(repr.base),
+                *tl2.head.of(repr.base)
+        );
+    };
+};
