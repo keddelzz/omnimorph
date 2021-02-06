c5540d6162815e4a2c22eeebc4f9405ee6ffa8c6
keddelzz <keddelzz@web.de>
2021-02-06 17:38:01 +0100

    Make Morpher work

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "hasmember.h"
 #include "show.h"
 
-/*
 template<typename Source>
 class Morpher
 {
@@ -17,24 +16,21 @@ public:
     template<typename Target>
     Target morph()
     {
-        using SGen = Generic<Source>;
-        using TGen = Generic<Target>;
-        using SMbs = typename SGen::Members;
-        using TMbs = typename TGen::Members;
+        using SGen = CopyGeneric<Source>;
+        using TGen = CopyGeneric<Target>;
         using SRep = typename SGen::Repr;
         using TRep = typename TGen::Repr;
         static_assert(
-            std::is_same_v<SMbs, TMbs>,
+            std::is_same_v<SRep, TRep>,
             "In order to morph two types their representations have to be the same!"
         );
 
-        const auto repr = SGen::to(source).members;
-        return TGen::from(TRep(nullptr, repr));
+        const auto repr = SGen::to(source);
+        return TGen::from(repr);
     }
 private:
     const Source &source;
 };
-*/
 
 int main() {
     SimpleAddress a("Nullptr Street", 55);
@@ -42,11 +38,14 @@ int main() {
     Vec3 t(1, 2, 3);
     Vac3 u(9, 8, 7);
 
+    auto x = Morpher(t).morph<Vac3>();
+
     std::cout
         << show(a) << std::endl
         << show(b) << std::endl
         << show(t) << std::endl
         << show(u) << std::endl
+        << show(x) << std::endl
         ;
 
     return EXIT_SUCCESS;
