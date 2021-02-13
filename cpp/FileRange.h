412d4af96423994f2623c9ceebdcb780f0896677
keddelzz <keddelzz@web.de>
2021-02-13 09:14:16 +0100

    Introduce classes representing file positions

---
+++ cpp/FileRange.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include "types.h"
+#include "FilePosition.h"
+
+struct FileRange
+{
+    explicit FileRange() = default;
+    explicit FileRange(
+        std::string fileName,
+        const FilePosition &start,
+        const FilePosition &end
+    );
+
+    std::string  fileName;
+    FilePosition start;
+    FilePosition end;
+
+    bool isNullRange() const;
+};
+
+std::ostream &operator<<(std::ostream &stream, const FileRange &range);
