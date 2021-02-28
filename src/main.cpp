72cb5122d4b15acc92e0e2a4cb1a534b5825627b
keddelzz <keddelzz@web.de>
2021-02-28 16:03:44 +0100

    Let CMake first build omnimorph and then the examples

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,29 @@
 
 #include "gg/Omnimorph.h"
 
+/*
 int main()
 {
-    const String inputFile("../../example/Fid.h");
-    const String outputFile("../../example/Fid.h.generic");
+    const String inputFile("../../example-old/Fid.h");
+    const String outputFile("../../example-old/Fid.h.generic");
+    gg::Omnimorph::generateGeneric(inputFile, outputFile);
+    return EXIT_SUCCESS;
+}
+*/
+
+int main(int argc, char *args[])
+{
+    constexpr auto expectedArgumentCount = 1 + 2;
+    const String programPath(args[0]);
+
+    if (argc != expectedArgumentCount) {
+        std::cout << "Syntax:" << std::endl;
+        std::cout << programPath << " <input-file> <output-file>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const String inputFile(args[1]);
+    const String outputFile(args[2]);
     gg::Omnimorph::generateGeneric(inputFile, outputFile);
     return EXIT_SUCCESS;
 }
