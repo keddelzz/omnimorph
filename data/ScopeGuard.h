5bf7a5a9ee7c634f52b67aa30e7534d9bf5aa97a
keddelzz <keddelzz@web.de>
2021-02-28 09:11:41 +0100

    Introduce an dis-/ableable scope guard

--- data/ScopeGuard.h
+++ data/ScopeGuard.h
@@ -16,3 +16,53 @@ public:
 private:
     Callback m_callback;
 };
+
+template<typename Callback>
+class EnabledScopeGuard
+{
+public:
+    EnabledScopeGuard(Callback callback)
+        : m_callback(callback)
+    {}
+
+    ~EnabledScopeGuard()
+    {
+        if (not m_disabled) {
+            m_callback();
+        }
+    }
+
+    void disable()
+    {
+        m_disabled = true;
+    }
+
+private:
+    bool m_disabled { false };
+    Callback m_callback;
+};
+
+template<typename Callback>
+class DisabledScopeGuard
+{
+public:
+    DisabledScopeGuard(Callback callback)
+        : m_callback(callback)
+    {}
+
+    ~DisabledScopeGuard()
+    {
+        if (m_enabled) {
+            m_callback();
+        }
+    }
+
+    void enable()
+    {
+        m_enabled = true;
+    }
+
+private:
+    bool m_enabled { false };
+    Callback m_callback;
+};
