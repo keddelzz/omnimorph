2e4601aa184cf707e49b34a99fafd5cee68ce265
keddelzz <keddelzz@web.de>
2021-02-28 13:31:28 +0100

    Remove unneeded code from main

--- main.cpp
+++ main.cpp
@@ -1,35 +1,8 @@
-#include <cstdlib>
 #include <iostream>
-
-#include "data/IterationDecision.h"
-
-#include "cpp/FileUtils.h"
-#include "cpp/Ast.h"
-#include "cpp/CppParser.h"
 
 #include "gg/Omnimorph.h"
 
-int main_()
-{
-    std::cout << "Hello, omnimorph!" << std::endl;
-
-    const String filePath("../example/Vec4f.h");
-    const auto fileContents = cpp::FileUtils::readEntireFile(filePath);
-
-    using namespace cpp;
-    CppParser parser;
-    parser.initialize(filePath, fileContents);
-    parser.foreachToplevelDeclaration([](Decl *decl) {
-        StringBuilder buffer;
-        Ast::showStructure(buffer, decl);
-        std::cout << buffer.toString() << std::endl;
-        return IterationDecision::Continue;
-    });
-
-    return EXIT_SUCCESS;
-}
-
-int main(int argc, char *argv[])
+int main()
 {
     const String inputFile("../example/Fid.h");
     StringBuilder buffer;
