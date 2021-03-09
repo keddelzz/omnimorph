a2c69b84970fccde48cdfbc69c84150f4e7358c6
keddelzz <keddelzz@web.de>
2021-03-09 20:00:34 +0100

    Turning arbitrary values into json for 'generic-json'

--- example/generic-json/main.cpp
+++ example/generic-json/main.cpp
@@ -8,6 +8,14 @@
 #include "json/Show.h"
 
 #include "Individual.h"
+#include "Individual.h.generic"
+
+#include "../common/Fid.h"
+#include "../common/Fid.h.generic"
+#include "../common/Vec2i.h"
+#include "../common/Vec2i.h.generic"
+#include "../common/Nested.h"
+#include "../common/Nested.h.generic"
 
 json::Value jsonTestIndividual()
 {
@@ -33,15 +41,61 @@ Individual testIndividual()
     return individual;
 }
 
+String showJson(const json::Value &json)
+{
+    StringBuilder buffer;
+    json::Show::json(buffer, json);
+    return buffer.toString();
+}
+
+void printJson(const json::Value &json)
+{
+    std::cout << showJson(json) << std::endl;
+}
+
+json::Value to_json(bool value) { return json::Value(value); }
+json::Value to_json(int value) { return json::Value(int64_t(value)); }
+json::Value to_json(double value) { return json::Value(value); }
+json::Value to_json(const String &value) { return json::Value(value); }
+template<typename T>
+json::Value to_json(const List<T> &value)
+{
+    json::Array array;
+    for (uint64_t i = 0; i < value.length; ++i) {
+        array.append(to_json(value[i]));
+    }
+    return json::Value(array);
+}
+
+void to_json_members(json::Object &object, const HNil &) {}
+template<typename Head, typename Tail>
+void to_json_members(json::Object &object, const HList<Named<const Head&>, Tail> &members)
+{
+    const auto &member = members.head;
+    object.insert(member.name, to_json(member.value));
+    to_json_members(object, members.tail);
+}
+
+template<typename T>
+json::Value to_json(const T &value)
+{
+    using Generic = ConstReferenceLabelledGeneric<T>;
+    const auto repr = Generic::to(value);
+
+    json::Object object;
+    object.insert("_type", String(Generic::name));
+    to_json_members(object, repr);
+    return json::Value(object);
+}
 int main() {
     std::cout << "Hello, generic-json" << std::endl;
 
-    const auto jsonIndividual = jsonTestIndividuals();
-//    const auto individual = testIndividual();
+    const auto individual = testIndividual();
+    const auto jsonIndividual = to_json(individual);
+    printJson(jsonIndividual);
 
-    StringBuilder buffer;
-    json::Show::json(buffer, jsonIndividual);
-    std::cout << buffer.toString() << std::endl;
+    const Fid fid { 13.37f, 55, 42.1337 };
+    const auto jsonFid = to_json(fid);
 
     return EXIT_SUCCESS;
 }
